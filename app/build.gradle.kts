5e397c9fe343fed4b8c76898c0d94ecfae12ca36
NankouFuraku <toatodd@gmail.com>
2025-04-14 14:17:43 +0300

    Add "name" handler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 application {
-    mainClass.set("org.example.hexlet.HelloWorld")
+    mainClass.set("org.example.HelloWorld")
 }
 
 group = "org.example"
--- app/src/main/java/org/example/HelloWorld.java
+++ app/src/main/java/org/example/HelloWorld.java
@@ -4,10 +4,17 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
+        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+
+        app.get("/users", ctx -> ctx.result("GET /users"));
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.post("/users", ctx -> ctx.result("POST /users"));
+
         app.start(7070);
     }
 }
